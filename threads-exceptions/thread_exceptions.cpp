3522a532ec5a8ecdb8128c9de119cabe29f5f069
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-09-29 13:48:52 +0200

    Threads & Exceptions

--- threads-exceptions/thread_exceptions.cpp
+++ threads-exceptions/thread_exceptions.cpp
@@ -8,28 +8,62 @@
 
 using namespace std::literals;
 
-void background_work(size_t id, const std::string& text, char& result)
+void background_work(size_t id, const std::string& text, 
+                     char& result, std::exception_ptr& eptr)
 {
-    std::cout << "bw#" << id << " has started..." << std::endl;
-
-    for (const auto& c : text)
+    try
     {
-        std::cout << "bw#" << id << ": " << c << std::endl;
+        std::cout << "bw#" << id << " has started..." << std::endl;
 
-        std::this_thread::sleep_for(100ms);
+        for (const auto& c : text)
+        {
+            std::cout << "bw#" << id << ": " << c << std::endl;
+
+            std::this_thread::sleep_for(100ms);
+        }
+
+        result = text.at(5); // potential exception
+
+        std::cout << "bw#" << id << " is finished..." << std::endl;
     }
-
-    result = text.at(5); // potential exception
-
-    std::cout << "bw#" << id << " is finished..." << std::endl;
+    catch(...)
+    {
+        eptr = std::current_exception();
+    }   
 }
 
 int main()
 {
     std::cout << "Main thread starts..." << std::endl;
-    const std::string text = "Hello Threads";
+    const std::string text = "Hello";
+    
+    char result = 0;
+    std::exception_ptr eptr;
 
-    // TODO
+    {
+        std::jthread thd_1{background_work, 1, std::cref(text), 
+                           std::ref(result), std::ref(eptr) };
+    }
+
+    if (eptr)
+    {
+        try
+        {
+            std::rethrow_exception(eptr);
+        }
+        catch(const std::out_of_range& e)
+        {
+            std::cout << "Caught an exception: " << e.what() << "\n";
+        }
+        catch(const std::exception& e)
+        {
+            std::cout << "Std exception caught: " << e.what() << "\n";
+        }
+    }
+    else
+    {
+        std::cout << "Result: " << result << "\n";
+    }
 
     std::cout << "Main thread ends..." << std::endl;
 }
