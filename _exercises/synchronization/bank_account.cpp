402c0ed006028a1bb4bf65f8f8604e940057661e
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-09-30 08:55:45 +0000

    BankAccount synchronized

--- _exercises/synchronization/bank_account.cpp
+++ _exercises/synchronization/bank_account.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
 
 class BankAccount
 {
     const int id_;
     double balance_;
+    mutable std::mutex mtx_balance_;
 
 public:
     BankAccount(int id, double balance)
@@ -15,22 +17,32 @@ public:
 
     void print() const
     {
-        std::cout << "Bank Account #" << id_ << "; Balance = " << balance_ << std::endl;
+        std::cout << "Bank Account #" << id_ << "; Balance = " << balance() << std::endl;
     }
 
     void transfer(BankAccount& to, double amount)
     {
+        #if __cplusplus < 201703L
+        std::unique_lock lk_from(mtx_balance_, std::defer_lock);
+        std::unique_lock lk_to(to.mtx_balance_, std::defer_lock);
+        std::lock(lk_from, lk_to);
+        #else
+        std::scoped_lock lk{mtx_balance_, to.mtx_balance_};
+        #endif  
+
         balance_ -= amount;
         to.balance_ += amount;
     }
 
     void withdraw(double amount)
     {
+        std::lock_guard lk{mtx_balance_};
         balance_ -= amount;
     }
 
     void deposit(double amount)
     {
+        std::lock_guard lk{mtx_balance_};
         balance_ += amount;
     }
 
@@ -41,6 +53,7 @@ public:
 
     double balance() const
     {
+        std::lock_guard lk{mtx_balance_};
         return balance_;
     }
 };
@@ -57,6 +70,12 @@ void make_deposits(BankAccount& ba, int no_of_operations)
         ba.deposit(1.0);
 }
 
+void make_transfers(BankAccount& ba_from, BankAccount& ba_to, int no_of_operations)
+{
+    for (int i = 0; i < no_of_operations; ++i)
+        ba_from.transfer(ba_to, 1.0);
+}
+
 int main()
 {
     const int NO_OF_ITERS = 10'000'000;
@@ -70,9 +89,13 @@ int main()
 
     std::thread thd1(&make_withdraws, std::ref(ba1), NO_OF_ITERS);
     std::thread thd2(&make_deposits, std::ref(ba1), NO_OF_ITERS);
+    std::thread thd3(&make_transfers, std::ref(ba1), std::ref(ba2), NO_OF_ITERS);
+    std::thread thd4(&make_transfers, std::ref(ba2), std::ref(ba1), NO_OF_ITERS);
 
     thd1.join();
     thd2.join();
+    thd3.join();
+    thd4.join();
 
     std::cout << "After all threads are done: ";
     ba1.print();
--- synchronization-locking/locking.cpp
+++ synchronization-locking/locking.cpp
@@ -17,18 +17,109 @@ void run(int& value)
     }
 }
 
+void may_throw()
+{
+    throw std::runtime_error("ERROR#13");
+}
+
+void run(int& value, std::mutex& mtx_value)
+{
+    for(int i = 0; i < 10'000'000; ++i)
+    {
+        mtx_value.lock();
+        std::lock_guard lk{mtx_value, std::adopt_lock}; // mtx_value.lock(); // start of critical section
+        ++value;
+        //may_throw();
+    } // mtx_value.unlock(); // end of critical section
+}
+
+void run(std::atomic<int>& value)
+{
+    for(int i = 0; i < 10'000'000; ++i)
+    {
+        ++value;
+    }
+}
+
+constexpr uintmax_t factorial(uintmax_t n)
+{
+    if (n <= 1)
+        return 1;
+    return n * factorial(n-1);
+}
+
+template <size_t N>
+constexpr auto create_factorial_lookup_table()
+{
+    std::array<uintmax_t, N> results{};
+
+    for(size_t i = 0; i < N; ++i)
+        results[i] = factorial(i);
+
+    return results;
+}
+
+void test_lookup_table()
+{
+    //constexpr int test_r = hardening_code();
+    constexpr auto lookup_factorial = create_factorial_lookup_table<13>();
+}
+
 int main()
 {
     std::cout << "Main thread starts..." << std::endl;
 
-    int counter = 0;
-    
+    // {
+    //     auto start = std::chrono::high_resolution_clock::now();
+
+    //     int counter = 0;
+
+    //     {
+    //         std::jthread thd_1{[&counter] { run(counter); }};
+    //         std::jthread thd_2{[&counter] { run(counter); }};
+    //     }
+
+    //     auto end = std::chrono::high_resolution_clock::now();
+
+    //     std::cout << "counter: " << counter << "\n";
+    //     std::cout << "time:" << std::chrono::duration_cast<std::chrono::milliseconds>(end - start) << "\n";
+    // }
+
+    std::cout << "-------------\n";
+
     {
-        std::jthread thd_1{[&counter] { run(counter); }};
-        std::jthread thd_2{[&counter] { run(counter); }};
+        auto start = std::chrono::high_resolution_clock::now();
+
+        int counter = 0;
+        std::mutex mtx_counter;
+
+        {
+            std::jthread thd_1{[&counter, &mtx_counter] { run(counter, mtx_counter); }};
+            std::jthread thd_2{[&counter, &mtx_counter] { run(counter, mtx_counter); }};
+        }
+
+        auto end = std::chrono::high_resolution_clock::now();
+
+        std::cout << "counter: " << counter << "\n";
+        std::cout << "time:" << std::chrono::duration_cast<std::chrono::milliseconds>(end - start) << "\n";
     }
 
-    std::cout << "counter: " << counter << "\n";
+    std::cout << "-------------\n";
+
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+
+        std::atomic<int> counter = 0;
+        {
+            std::jthread thd_1{[&counter] { run(counter); }};
+            std::jthread thd_2{[&counter] { run(counter); }};
+        }
+
+        auto end = std::chrono::high_resolution_clock::now();
+
+        std::cout << "counter: " << counter << "\n";
+        std::cout << "time:" << std::chrono::duration_cast<std::chrono::milliseconds>(end - start) << "\n";
+    }
 
     std::cout << "Main thread ends..." << std::endl;
 }
