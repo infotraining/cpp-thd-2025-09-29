c4d6dc430cd569d50fb7a545be4dabbb105e62d8
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-10-01 08:45:52 +0200

    Event synchronization with atomics - 1

--- event-synchronization/event_synchronization.cpp
+++ event-synchronization/event_synchronization.cpp
@@ -14,13 +14,16 @@ using namespace std::literals;
 
 namespace EventsWithConditionVars
 {
-    class Data {
+    class Data
+    {
         std::vector<int> data_;
         bool is_ready_ = false;
         std::mutex mtx_is_ready_;
         std::condition_variable cv_data_ready_;
+
     public:
-        void read() {
+        void read()
+        {
             std::osyncstream(std::cout) << "Start reading..." << std::endl;
             data_.resize(100);
 
@@ -36,7 +39,8 @@ namespace EventsWithConditionVars
             cv_data_ready_.notify_all();
         }
 
-        void process(int id) {
+        void process(int id)
+        {
             std::unique_lock lk{mtx_is_ready_};
             cv_data_ready_.wait(lk, [&] { return is_ready_; });
             lk.unlock();
@@ -89,13 +93,42 @@ namespace ConditionVarsWithStopToken
             std::osyncstream(std::cout) << "Id: " << id << "; Sum: " << sum << std::endl;
         }
     };
-} // namespace EventsWithConditionVars
+} // namespace ConditionVarsWithStopToken
 
+class Data
+{
+    std::vector<int> data_;
+    std::atomic<bool> is_ready_ = false;
+    static_assert(std::atomic<double>::is_always_lock_free);
+public:
+    void read()
+    {
+        std::osyncstream(std::cout) << "Start reading..." << std::endl;
+        data_.resize(100);
+        
+        std::random_device rnd;
+        std::generate(begin(data_), end(data_), [&rnd] { return rnd() % 1000; });
+        std::this_thread::sleep_for(2s);
+        std::osyncstream(std::cout) << "End reading..." << std::endl;
+        
+        /////////////////////////////////////////////////////////
+        is_ready_ = true; 
+    }
+
+    void process(int id)
+    {
+        while (!is_ready_) //////////////////////////////////////
+        {
+            //std::this_thread::yield();
+        }
+        
+        long sum = std::accumulate(begin(data_), end(data_), 0L);
+        std::osyncstream(std::cout) << "Id: " << id << "; Sum: " << sum << std::endl;
+    }
+};
 
 int main()
 {
-    using ConditionVarsWithStopToken::Data;
-
     std::osyncstream(std::cout) << "Start of main..." << std::endl;
     {
         Data data;
@@ -104,8 +137,8 @@ int main()
 
         std::stop_source stop_src;
 
-        std::jthread thd_consumer_1{[&] { data.process(1, stop_src.get_token()); }};
-        std::jthread thd_consumer_2{[&] { data.process(2, stop_src.get_token()); }};
+        std::jthread thd_consumer_1{[&] { data.process(1); }};
+        std::jthread thd_consumer_2{[&] { data.process(2); }};
 
         std::this_thread::sleep_for(3s);
         stop_src.request_stop();
